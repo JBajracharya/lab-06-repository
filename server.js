a57750554d92f6aba2928206dc1e563887ae9125
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-10 10:56:41 -0800

    made connection to server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const cors = require('cors');
 
@@ -11,5 +11,12 @@ require('dotenv').config();
 app.use(cors());
 
 app.get('/location', (request, response) => {
+    // const getData = require('./data/geo.json');
+    let testData = {msg: 'HELLO'};
     
+    response.send(testData);
 })
+
+app.listen(PORT, () => {
+    console.log(`app is up on PORT: ${PORT}`);
+  });
