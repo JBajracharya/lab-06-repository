ed16f1d3763a188b72893b73654780103197b393
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-12-10 13:24:01 -0800

    finished upto no.2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
 
@@ -10,11 +10,43 @@ require('dotenv').config();
 
 app.use(cors());
 
+function Location (locName, lat, long) {
+    this.locName = locName;
+    this.lat = lat;
+    this.long = long;
+}
+
+
+
 app.get('/location', (request, response) => {
-    // const getData = require('./data/geo.json');
-    let testData = {msg: 'HELLO'};
     
-    response.send(testData);
+    const geoData = require('./data/geo.json');
+    console.log(geoData);
+
+    const geoDataResult = geoData.results[0].address_components[0].long_name;
+    console.log(geoDataResult);
+
+    const geoFormattedQuery = geoData.results[0].formatted_address;
+    
+    const lat = geoData.results[0].geometry.location.lat;
+
+    const long = geoData.results[0].geometry.location.lng;
+
+    response.send(lat);
+
+    // response.send({location : {
+    //     'search_query': 'seattle',
+    //     'formatted_query': 'Seattle, WA, USA',
+    //     'latitude': '47.606210',
+    //     'longitude': '-122.332071'
+    //   } });
+
+
+    // response.send(getData);
+    // let testData = {msg: 'HELLO'};
+    
+    // response.send(location);
+    // const location = new Location('seattle','121212', '121211');
 })
 
 app.listen(PORT, () => {
